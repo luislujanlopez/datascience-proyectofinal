3481ab4e297ea4475535779c2fd4fa4264d618b0
Luis Lujan Lopez <luis.lujan.lopez@outlook.com>
2025-10-06 16:37:27 -0500

    feat: Se agrego nuevos campos a Producto: Descripcion, Modelo y urlImagen

--- ProyectoFinalWebApi/Models/Producto.cs
+++ ProyectoFinalWebApi/Models/Producto.cs
@@ -5,5 +5,8 @@
         public int Id { get; set; }
         public string Nombre { get; set; }
         public decimal Precio { get; set; }
+        public string Descripcion { get; set; }
+        public string Modelo { get; set; }
+        public string urlImagen { get; set; }
     }
 }
