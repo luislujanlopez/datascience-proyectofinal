f4c9bb303201dcdd6499644cdc1ef2ee4e8affed
Luis Lujan Lopez <luis.lujan.lopez@outlook.com>
2025-10-06 16:45:54 -0500

    feat: Se agrego nuevos campos a usuario

--- ProyectoFinalWebApi/DTOs/UsuarioLoginDto.cs
+++ ProyectoFinalWebApi/DTOs/UsuarioLoginDto.cs
@@ -4,5 +4,7 @@
     {
         public string Usuario { get; set; }
         public string Password { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }        
     }
 }
